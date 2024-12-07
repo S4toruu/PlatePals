45c424816468d54d994079fcd5f50964f38ce486
Romain Couturier <r.couturier@quadient.com>
2024-12-07 13:25:19 +0100

    WiP: Adds base for recipe rating

--- components/entities/recipe--teaser.tsx
+++ components/entities/recipe--teaser.tsx
@@ -1,7 +1,9 @@
+import React, { useState } from 'react'
+import { getSession } from 'next-auth/react'
+import { Session } from 'next-auth'
 import { Recipe } from '@prisma/client'
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
 import {
   mdiStar, mdiStarHalfFull, mdiStarOutline
 } from '@mdi/js'
@@ -13,9 +15,25 @@ export interface recipeTeaserProps {
 }
 
 export function RecipeTeaser({ recipe }: recipeTeaserProps) {
+  const [session, setSession] = useState<Session | null>(null)
+  const [hoveredIndex, setHoveredIndex] = useState<number | null>(null)
   const fullStars = Math.floor(recipe.averageStars)
   const halfStar = recipe.averageStars % 1 >= 0.5
-  const emptyStars = 5 - fullStars - (halfStar ? 1 : 0)
+
+  getSession().then((sess) => {
+    setSession(sess)
+  })
+
+  const handleRatingClick = (index: number) => {
+    const rating = index + 1
+    // todo Saves the rating to the database
+    console.log(`Rating saved: ${rating}`)
+    if (session) {
+      // todo POST request to save rating
+    } else {
+      // todo display login modal
+    }
+  }
 
   return (
     <div className={styles.recipeTeaser}>
@@ -30,18 +48,22 @@ export function RecipeTeaser({ recipe }: recipeTeaserProps) {
           />
         </figure>
         <h3>{recipe.title}</h3>
-        <ul className={styles.rank}>
-          {[...Array(fullStars)].map((_, i) => (
-            // eslint-disable-next-line react/no-array-index-key
-            <li key={`full-star-${i}`}><Icon path={mdiStar} size={1} /></li>
-          ))}
-          {halfStar && <li key="half-star"><Icon path={mdiStarHalfFull} size={1} /></li>}
-          {[...Array(emptyStars)].map((_, i) => (
-            // eslint-disable-next-line react/no-array-index-key
-            <li key={`empty-star-${i}`}><Icon path={mdiStarOutline} size={1} /></li>
-          ))}
-        </ul>
       </Link>
+      <ul
+        className={styles.rank}
+        onMouseLeave={() => setHoveredIndex(null)}
+      >
+        {[...Array(5)].map((_, i) => (
+          <li
+            // eslint-disable-next-line react/no-array-index-key
+            key={`star-${i}`}
+            onMouseEnter={() => setHoveredIndex(i)}
+            onClick={() => handleRatingClick(i)}
+          >
+            <Icon path={hoveredIndex !== null && i <= hoveredIndex ? mdiStar : (i < fullStars ? mdiStar : (i === fullStars && halfStar ? mdiStarHalfFull : mdiStarOutline))} size={1} />
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
